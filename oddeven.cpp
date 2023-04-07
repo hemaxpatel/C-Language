5c752e5ab598347b29c700e15dfda50666b11ee7
hemaxpatel <120293676+hemaxpatel@users.noreply.github.com>
2023-04-07 17:57:23 +0530

    Add files via upload

---
+++ oddeven.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int n;
+    cin>>n;
+    if(n%2==0)
+        cout<<"Given number is even number";
+    else
+        cout<<"Given number is odd number";
+}
